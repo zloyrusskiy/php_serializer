a4bfcc1d1e99fcab4d8f5df6b96ff140eb6e63f9
Alexandr Fedorov <alexandrfedorov@gmail.com>
2017-03-18 23:10:29 +0400

    Initial commit

---
+++ lib/php_serializer.ex
@@ -0,0 +1,18 @@
+defmodule PhpSerializer do
+  @moduledoc """
+  Documentation for PhpSerializer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhpSerializer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/php_serializer_test.exs
@@ -0,0 +1,8 @@
+defmodule PhpSerializerTest do
+  use ExUnit.Case
+  doctest PhpSerializer
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
